c93b2b12eeb703b193b1a72d412c98eed0d2636b
Syed Usama <usamaqadeer.te@gmail.com>
2025-07-30 19:01:07 +0500

    added cpde

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.main');
 });
 
 Route::get('/admin/login',[AdminController::class,'login'])->name('admin.login');
+Route::get('/dashboard',[AdminController::class,'login'])->middleware(['auth', 'verified'])->name('dashboard');;
 
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
