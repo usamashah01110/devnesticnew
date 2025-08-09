42e740c7bd17ffb3b8091bde22ad15c524d2ef95
Syed Usama <usamaqadeer.te@gmail.com>
2025-08-09 18:18:01 +0500

    pushed code

--- app/Models/sectionFive.php
+++ app/Models/sectionFive.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class sectionFive extends Model
 {
     use HasFactory;
+
+    protected $table = 'section_fives';
+
     protected $fillable = [
         'name',
         'description',
