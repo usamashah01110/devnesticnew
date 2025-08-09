9512c1b5e5e04cee9350373a94f9e16df9704927
Syed Usama <usamaqadeer.te@gmail.com>
2025-08-09 18:21:17 +0500

    fix the issue of sectio

--- app/Models/SectionFive.php
+++ app/Models/SectionFive.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class sectionFive extends Model
+class SectionFive extends Model
 {
     use HasFactory;
 
