f1071f60f1856d088d13dc12b1bcf59cd3433def
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-08-20 15:41:33 -0700

    edit interface and repository for section nine

--- app/Repositories/SectionNineRepository.php
+++ app/Repositories/SectionNineRepository.php
@@ -3,7 +3,34 @@
 namespace App\Repositories;
 
 use App\Repositories\Interfaces\SectionNineRepositoryInterface;
+use App\Models\SectionNine;
 
-class SectionNineRepository implements SectionNineRepositoryInterface {
-    
+class SectionNineRepository implements SectionNineRepositoryInterface
+{
+    protected $model;
+
+    public function __construct(SectionNine $model)
+    {
+        $this->model = $model;
+    }
+    public function all()
+    {
+        return $this->model->all();
+    }
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+    public function update($id, array $data)
+    {
+        return $this->model->find($id)->update($data);
+    }
+    public function delete($id)
+    {
+        return $this->model->find($id)->delete();
+    }
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
 }
