125bb8e017e07c62964801dc7bb5faf809d2d367
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-09-08 17:40:56 -0700

    Add code

---
+++ app/Http/Controllers/DeveloperEducation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\BaseController;
+
+use App\Http\Controllers\BaseController;
+use App\Repositories\DeveloperEducationRepository;
+use Illuminate\Http\Request;
+
+class DeveloperEducation extends BaseController
+{
+    public function __construct(DeveloperEducationRepository )
+    
+}
